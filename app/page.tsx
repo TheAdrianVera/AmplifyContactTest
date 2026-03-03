4c2e4cbd1128308cc4bbae5ac9988455e8b56279
TheAdrianVera <agvm94@gmail.com>
2026-03-02 20:21:26 -0600

    Removed bg-white

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 bg-white bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 bg-black sm:items-start">
         <a
           href="contact-test"
           className="inline-flex items-center justify-center rounded-lg bg-orange-600 px-10 py-5 text-md font-bold text-white shadow-sm transition hover:bg-orange-700 hover:shadow-md hover:-translate-y-0.5"
